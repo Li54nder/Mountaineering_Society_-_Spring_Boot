40c653c75a69507ced1bfd480180dc35992b7354
Aleksa <aleksa.novkovic98@gmail.com>
2020-01-25 22:45:38 +0100

    Database Data

---
+++ PD-Data Dump/Planinarsko drustvo_Znamenitost.sql
@@ -0,0 +1,35 @@
+-- MariaDB dump 10.17  Distrib 10.4.11-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: Planinarsko drustvo
+-- ------------------------------------------------------
+-- Server version	10.4.11-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `Znamenitost`
+--
+
+LOCK TABLES `Znamenitost` WRITE;
+/*!40000 ALTER TABLE `Znamenitost` DISABLE KEYS */;
+INSERT INTO `Znamenitost` VALUES (1,0,'Vidikovac','Vidikovac Nebeske Stolice',1),(2,1,'Izletiste','Izletiste Djorov most',2),(3,0,'Vodopad','Vodopadi Jelovarnik',3),(4,0,'Spomenik','Spomenik streljanim partizanima, mesto gde su nemci 29. novembra 1941. godine, zatecene ranjene i nenaoruzane partizane gazili tenkovima i pucali na njih.',4),(5,1,'Vidikovac','Vidikovac Crnjeskovo se nalazi na 1035 metara nadmorske visine. Pogled se pruza na klisuru reke Race i Bajinu Bastu',5);
+/*!40000 ALTER TABLE `Znamenitost` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-25 22:29:26
